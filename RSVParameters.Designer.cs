4939a1df5b5bbfe59f81679e866408d66a4b863a
islamahmed9717 <119157423+islamahmed9717@users.noreply.github.com>
2024-03-06 17:14:41 +0200

    Add files via upload

---
+++ RSVParameters.Designer.cs
@@ -0,0 +1,468 @@
+﻿namespace ThrusterTest.UserControls
+{
+    partial class RSVParameters
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblProfile = new Label();
+            cmbProfiles = new ComboBox();
+            btnNew = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox2 = new GroupBox();
+            txtDraft = new TextBox();
+            txtCBZ = new TextBox();
+            txtCBY = new TextBox();
+            txtCBX = new TextBox();
+            txtCGZ = new TextBox();
+            txtCGY = new TextBox();
+            txtCGX = new TextBox();
+            lblDraft = new Label();
+            lblCBshift = new Label();
+            lblCGshift = new Label();
+            lblZ = new Label();
+            lblY = new Label();
+            lblX = new Label();
+            lbl4 = new Label();
+            lbl3 = new Label();
+            lbl2 = new Label();
+            lbl1 = new Label();
+            txtMass = new TextBox();
+            txtBallast2 = new TextBox();
+            txtBallast1 = new TextBox();
+            txtFuelLevel = new TextBox();
+            lblMass = new Label();
+            lblBallast2 = new Label();
+            lblBallast1 = new Label();
+            lblFuelLevel = new Label();
+            txtProfileName = new TextBox();
+            label1 = new Label();
+            btnSave = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblProfile
+            // 
+            lblProfile.AutoSize = true;
+            lblProfile.Location = new Point(51, 25);
+            lblProfile.Name = "lblProfile";
+            lblProfile.Size = new Size(41, 15);
+            lblProfile.TabIndex = 0;
+            lblProfile.Text = "Profile";
+            // 
+            // cmbProfiles
+            // 
+            cmbProfiles.FormattingEnabled = true;
+            cmbProfiles.Location = new Point(119, 22);
+            cmbProfiles.Name = "cmbProfiles";
+            cmbProfiles.Size = new Size(328, 23);
+            cmbProfiles.TabIndex = 1;
+            cmbProfiles.SelectedIndexChanged += cmbProfiles_SelectedIndexChanged;
+            // 
+            // btnNew
+            // 
+            btnNew.Location = new Point(501, 22);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(86, 24);
+            btnNew.TabIndex = 2;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = true;
+            btnNew.Click += btnNew_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(610, 22);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(86, 24);
+            btnEdit.TabIndex = 3;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(708, 22);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(86, 24);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox3);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(txtProfileName);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(25, 63);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(770, 399);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Profile Settings";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(397, 60);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(368, 333);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Extrinsic Parameters";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtDraft);
+            groupBox2.Controls.Add(txtCBZ);
+            groupBox2.Controls.Add(txtCBY);
+            groupBox2.Controls.Add(txtCBX);
+            groupBox2.Controls.Add(txtCGZ);
+            groupBox2.Controls.Add(txtCGY);
+            groupBox2.Controls.Add(txtCGX);
+            groupBox2.Controls.Add(lblDraft);
+            groupBox2.Controls.Add(lblCBshift);
+            groupBox2.Controls.Add(lblCGshift);
+            groupBox2.Controls.Add(lblZ);
+            groupBox2.Controls.Add(lblY);
+            groupBox2.Controls.Add(lblX);
+            groupBox2.Controls.Add(lbl4);
+            groupBox2.Controls.Add(lbl3);
+            groupBox2.Controls.Add(lbl2);
+            groupBox2.Controls.Add(lbl1);
+            groupBox2.Controls.Add(txtMass);
+            groupBox2.Controls.Add(txtBallast2);
+            groupBox2.Controls.Add(txtBallast1);
+            groupBox2.Controls.Add(txtFuelLevel);
+            groupBox2.Controls.Add(lblMass);
+            groupBox2.Controls.Add(lblBallast2);
+            groupBox2.Controls.Add(lblBallast1);
+            groupBox2.Controls.Add(lblFuelLevel);
+            groupBox2.Location = new Point(6, 60);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(368, 333);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Intrinsic Parameters";
+            // 
+            // txtDraft
+            // 
+            txtDraft.Location = new Point(88, 294);
+            txtDraft.Name = "txtDraft";
+            txtDraft.Size = new Size(213, 23);
+            txtDraft.TabIndex = 24;
+            // 
+            // txtCBZ
+            // 
+            txtCBZ.Location = new Point(234, 254);
+            txtCBZ.Name = "txtCBZ";
+            txtCBZ.Size = new Size(67, 23);
+            txtCBZ.TabIndex = 23;
+            // 
+            // txtCBY
+            // 
+            txtCBY.Location = new Point(161, 254);
+            txtCBY.Name = "txtCBY";
+            txtCBY.Size = new Size(67, 23);
+            txtCBY.TabIndex = 22;
+            // 
+            // txtCBX
+            // 
+            txtCBX.Location = new Point(88, 254);
+            txtCBX.Name = "txtCBX";
+            txtCBX.Size = new Size(67, 23);
+            txtCBX.TabIndex = 21;
+            // 
+            // txtCGZ
+            // 
+            txtCGZ.Location = new Point(234, 215);
+            txtCGZ.Name = "txtCGZ";
+            txtCGZ.Size = new Size(67, 23);
+            txtCGZ.TabIndex = 20;
+            // 
+            // txtCGY
+            // 
+            txtCGY.Location = new Point(161, 215);
+            txtCGY.Name = "txtCGY";
+            txtCGY.Size = new Size(67, 23);
+            txtCGY.TabIndex = 19;
+            // 
+            // txtCGX
+            // 
+            txtCGX.Location = new Point(88, 215);
+            txtCGX.Name = "txtCGX";
+            txtCGX.Size = new Size(67, 23);
+            txtCGX.TabIndex = 18;
+            // 
+            // lblDraft
+            // 
+            lblDraft.AutoSize = true;
+            lblDraft.Location = new Point(9, 297);
+            lblDraft.Name = "lblDraft";
+            lblDraft.Size = new Size(33, 15);
+            lblDraft.TabIndex = 17;
+            lblDraft.Text = "Draft";
+            // 
+            // lblCBshift
+            // 
+            lblCBshift.AutoSize = true;
+            lblCBshift.Location = new Point(9, 257);
+            lblCBshift.Name = "lblCBshift";
+            lblCBshift.Size = new Size(48, 15);
+            lblCBshift.TabIndex = 16;
+            lblCBshift.Text = "CB shift";
+            // 
+            // lblCGshift
+            // 
+            lblCGshift.AutoSize = true;
+            lblCGshift.Location = new Point(9, 215);
+            lblCGshift.Name = "lblCGshift";
+            lblCGshift.Size = new Size(49, 15);
+            lblCGshift.TabIndex = 15;
+            lblCGshift.Text = "CG shift";
+            // 
+            // lblZ
+            // 
+            lblZ.AutoSize = true;
+            lblZ.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblZ.Location = new Point(260, 183);
+            lblZ.Name = "lblZ";
+            lblZ.Size = new Size(16, 17);
+            lblZ.TabIndex = 14;
+            lblZ.Text = "Z";
+            // 
+            // lblY
+            // 
+            lblY.AutoSize = true;
+            lblY.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblY.Location = new Point(187, 183);
+            lblY.Name = "lblY";
+            lblY.Size = new Size(16, 17);
+            lblY.TabIndex = 13;
+            lblY.Text = "Y";
+            // 
+            // lblX
+            // 
+            lblX.AutoSize = true;
+            lblX.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblX.Location = new Point(114, 183);
+            lblX.Name = "lblX";
+            lblX.Size = new Size(17, 17);
+            lblX.TabIndex = 12;
+            lblX.Text = "X";
+            // 
+            // lbl4
+            // 
+            lbl4.AutoSize = true;
+            lbl4.Location = new Point(315, 151);
+            lbl4.Name = "lbl4";
+            lbl4.Size = new Size(21, 15);
+            lbl4.TabIndex = 11;
+            lbl4.Text = "Kg";
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.Location = new Point(315, 113);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new Size(17, 15);
+            lbl3.TabIndex = 10;
+            lbl3.Text = "%";
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Location = new Point(315, 70);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(17, 15);
+            lbl2.TabIndex = 9;
+            lbl2.Text = "%";
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(315, 33);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(17, 15);
+            lbl1.TabIndex = 8;
+            lbl1.Text = "%";
+            // 
+            // txtMass
+            // 
+            txtMass.Location = new Point(88, 147);
+            txtMass.Name = "txtMass";
+            txtMass.Size = new Size(221, 23);
+            txtMass.TabIndex = 7;
+            // 
+            // txtBallast2
+            // 
+            txtBallast2.Location = new Point(88, 109);
+            txtBallast2.Name = "txtBallast2";
+            txtBallast2.Size = new Size(221, 23);
+            txtBallast2.TabIndex = 6;
+            // 
+            // txtBallast1
+            // 
+            txtBallast1.Location = new Point(88, 66);
+            txtBallast1.Name = "txtBallast1";
+            txtBallast1.Size = new Size(221, 23);
+            txtBallast1.TabIndex = 5;
+            // 
+            // txtFuelLevel
+            // 
+            txtFuelLevel.Location = new Point(88, 29);
+            txtFuelLevel.Name = "txtFuelLevel";
+            txtFuelLevel.Size = new Size(221, 23);
+            txtFuelLevel.TabIndex = 4;
+            txtFuelLevel.KeyPress += FuelLevel;
+            // 
+            // lblMass
+            // 
+            lblMass.AutoSize = true;
+            lblMass.Location = new Point(9, 151);
+            lblMass.Name = "lblMass";
+            lblMass.Size = new Size(34, 15);
+            lblMass.TabIndex = 3;
+            lblMass.Text = "Mass";
+            // 
+            // lblBallast2
+            // 
+            lblBallast2.AutoSize = true;
+            lblBallast2.Location = new Point(9, 113);
+            lblBallast2.Name = "lblBallast2";
+            lblBallast2.Size = new Size(63, 15);
+            lblBallast2.TabIndex = 2;
+            lblBallast2.Text = "Ballast(aft)";
+            // 
+            // lblBallast1
+            // 
+            lblBallast1.AutoSize = true;
+            lblBallast1.Location = new Point(9, 70);
+            lblBallast1.Name = "lblBallast1";
+            lblBallast1.Size = new Size(73, 15);
+            lblBallast1.TabIndex = 1;
+            lblBallast1.Text = "Ballast (fore)";
+            // 
+            // lblFuelLevel
+            // 
+            lblFuelLevel.AutoSize = true;
+            lblFuelLevel.Location = new Point(9, 33);
+            lblFuelLevel.Name = "lblFuelLevel";
+            lblFuelLevel.Size = new Size(59, 15);
+            lblFuelLevel.TabIndex = 0;
+            lblFuelLevel.Text = "Fuel Level";
+            // 
+            // txtProfileName
+            // 
+            txtProfileName.Location = new Point(110, 31);
+            txtProfileName.Name = "txtProfileName";
+            txtProfileName.Size = new Size(312, 23);
+            txtProfileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Profile Name";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(712, 468);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(83, 27);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // RSVParameters
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnSave);
+            Controls.Add(groupBox1);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnNew);
+            Controls.Add(cmbProfiles);
+            Controls.Add(lblProfile);
+            Name = "RSVParameters";
+            Size = new Size(816, 506);
+            Load += RSVParameters_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblProfile;
+        private ComboBox cmbProfiles;
+        private Button btnNew;
+        private Button btnEdit;
+        private Button btnDelete;
+        private GroupBox groupBox1;
+        private Label label1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
+        private TextBox txtDraft;
+        private TextBox txtCBZ;
+        private TextBox txtCBY;
+        private TextBox txtCBX;
+        private TextBox txtCGZ;
+        private TextBox txtCGY;
+        private TextBox txtCGX;
+        private Label lblDraft;
+        private Label lblCBshift;
+        private Label lblCGshift;
+        private Label lblZ;
+        private Label lblY;
+        private Label lblX;
+        private Label lbl4;
+        private Label lbl3;
+        private Label lbl2;
+        private Label lbl1;
+        private TextBox txtMass;
+        private TextBox txtBallast2;
+        private TextBox txtBallast1;
+        private TextBox txtFuelLevel;
+        private Label lblMass;
+        private Label lblBallast2;
+        private Label lblBallast1;
+        private Label lblFuelLevel;
+        private TextBox txtProfileName;
+        private Button btnSave;
+    }
+}
---
+++ RSVParameters.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ThrusterTest.UserControls
+{
+    public partial class RSVParameters : UserControl
+    {
+        private static string path = AppDomain.CurrentDomain.BaseDirectory;
+        private static string filepath = Path.Combine(path, "profiles.xml");
+        private bool button1WasClicked = false;
+
+        public RSVParameters()
+        {
+
+            InitializeComponent();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("profiles.xml");
+
+            foreach (XmlNode node in doc.SelectNodes("//Profile/Name"))
+            {
+                cmbProfiles.Items.Add(node.InnerText);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                XmlDocument doc = new XmlDocument();
+
+                // Check if the XML file exists
+                if (File.Exists(filepath))
+                {
+                    doc.Load(filepath);
+                }
+                else
+                {
+                    XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                    doc.AppendChild(declaration);
+
+                    XmlElement root = doc.CreateElement("Profiles");
+                    doc.AppendChild(root);
+                }
+                string profileName = txtProfileName.Text;
+                if (ProfileExists(doc, profileName))
+                {
+                    MessageBox.Show("Profile name already exists! Please enter another name.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Exit the function if profile name already exists
+                }
+
+                XmlElement profile = doc.CreateElement("Profile");
+
+                // Save data from each text box to the XML file
+                SaveDataElement(doc, profile, "Name", txtProfileName.Text);
+                SaveDataElement(doc, profile, "Draft", txtDraft.Text);
+                SaveDataElement(doc, profile, "CBX", txtCBX.Text);
+                SaveDataElement(doc, profile, "CGZ", txtCGZ.Text);
+                SaveDataElement(doc, profile, "CGY", txtCGY.Text);
+                SaveDataElement(doc, profile, "CGX", txtCGX.Text);
+                SaveDataElement(doc, profile, "Mass", txtMass.Text);
+                SaveDataElement(doc, profile, "Ballast1", txtBallast1.Text);
+                SaveDataElement(doc, profile, "Ballast2", txtBallast2.Text);
+                SaveDataElement(doc, profile, "FuelLevel", txtFuelLevel.Text);
+
+                doc.DocumentElement.AppendChild(profile);
+                doc.Save(filepath);
+
+                cmbProfiles.Items.Add(txtProfileName.Text);
+
+                button1WasClicked = true;
+                if (button1WasClicked == true)
+                {
+                    cmbProfiles.Enabled = true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+
+        }
+
+        private bool ProfileExists(XmlDocument doc, string profileName)
+        {
+            // Use XPath to find profiles with the matching name
+            var nodes = doc.SelectNodes("//Profile[Name='" + profileName + "']");
+
+            // Return true if any nodes are found (meaning the profile exists)
+            return nodes.Count > 0;
+        }
+
+
+        private void SaveDataElement(XmlDocument doc, XmlElement parentElement, string elementName, string elementValue)
+        {
+            XmlElement element = doc.CreateElement(elementName);
+            element.InnerText = elementValue;
+            parentElement.AppendChild(element);
+        }
+
+        private void cmbProfiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbProfiles.SelectedItem != null)
+            {
+                string selectedName = cmbProfiles.SelectedItem.value ?? cmbProfiles.SelectedItem.ToString();
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filepath);
+
+                XmlNode profileNode = doc.SelectSingleNode($"//Profile[@Name='{selectedName}']");
+
+                if (profileNode != null)
+                {
+                   
+                    // ... (populate other text boxes)
+                    txtProfileName.Text = GetValueFromProfileNode(profileNode, "Name");
+                    txtDraft.Text = GetValueFromProfileNode(profileNode, "Draft");
+                    txtCBZ.Text = GetValueFromProfileNode(profileNode, "CBZ");
+                    txtCBY.Text = GetValueFromProfileNode(profileNode, "CBY");
+                    txtCBX.Text = GetValueFromProfileNode(profileNode, "CBX");
+                    txtCGZ.Text = GetValueFromProfileNode(profileNode, "CGZ");
+                    txtCGY.Text = GetValueFromProfileNode(profileNode, "CGY");
+                    txtCGX.Text = GetValueFromProfileNode(profileNode, "CGX");
+                    txtMass.Text = GetValueFromProfileNode(profileNode, "Mass");
+                    txtBallast1.Text = GetValueFromProfileNode(profileNode, "Ballast1");
+                    txtBallast2.Text = GetValueFromProfileNode(profileNode, "Ballast2");
+                    txtFuelLevel.Text = GetValueFromProfileNode(profileNode, "FuelLevel");
+                }
+                else
+                {
+                    // Handle case where profile not found:
+                    MessageBox.Show("Profile '" + selectedName + "' not found in the XML file.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    // Optional: Clear text boxes to avoid displaying old data
+                    txtProfileName.Clear();
+                    txtDraft.Clear();
+                    // ... clear other text boxes
+                }
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Please select a profile first");
+            }
+
+
+        }
+
+        private string GetValueFromProfileNode(XmlNode profileNode, string elementName)
+        {
+            if (profileNode != null)
+            {
+                XmlNode elementNode = profileNode.SelectSingleNode(elementName);
+                if (elementNode != null)
+                {
+                    return elementNode.InnerText;
+                }
+            }
+            return string.Empty;
+        }
+
+        private void FuelLevel(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null && textBox == txtFuelLevel)
+            {
+                // Allow only digits and decimal point for txtFuelLevel
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+                {
+                    e.Handled = true;
+                }
+
+                // Allow only one decimal point
+                if (e.KeyChar == '.' && textBox.Text.Contains("."))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Check if a profile is selected
+            if (cmbProfiles.SelectedItem != null)
+            {
+                // Load the XML document
+                XmlDocument doc = new XmlDocument();
+                doc.Load("profiles.xml");
+
+                // Get the selected profile name
+                string profileName = cmbProfiles.SelectedItem.ToString();
+
+                // Find the profile node in the XML
+                XmlNode profileNode = doc.SelectSingleNode($"//Profile[Name='{profileName}']");
+
+                if (profileNode != null)
+                {
+                    XmlNode node = profileNode;
+
+                    node.ParentNode.RemoveChild(node);
+
+                    doc.Save("profiles.xml");
+
+                    // Update the combobox
+                    cmbProfiles.Items.RemoveAt(cmbProfiles.SelectedIndex);
+                    cmbProfiles.SelectedIndex = -1;
+                    cmbProfiles.Text = "";
+                    cmbProfiles.Refresh();
+
+
+                    txtProfileName.Clear();
+                    txtMass.Clear();
+                    txtBallast1.Clear();
+                    txtBallast2.Clear();
+                    txtCBX.Clear();
+                    txtCBY.Clear();
+                    txtCBZ.Clear();
+                    txtCGX.Clear();
+                    txtCGY.Clear();
+                    txtCGZ.Clear();
+                    txtFuelLevel.Clear();
+                    txtFuelLevel.Clear();
+                    txtDraft.Clear();
+
+
+                    MessageBox.Show("Profile deleted successfully!", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Handle case where profile is not found
+                    MessageBox.Show("Profile not found!", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                // Handle case where no profile is selected
+                MessageBox.Show("Please select a profile to delete first!", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            cmbProfiles.SelectedIndex = -1;
+            cmbProfiles.Text = "";
+            cmbProfiles.Refresh();
+            txtProfileName.Clear();
+            txtMass.Clear();
+            txtBallast1.Clear();
+            txtBallast2.Clear();
+            txtCBX.Clear();
+            txtCBY.Clear();
+            txtCBZ.Clear();
+            txtCGX.Clear();
+            txtCGY.Clear();
+            txtCGZ.Clear();
+            txtFuelLevel.Clear();
+            txtFuelLevel.Clear();
+            txtDraft.Clear();
+
+        }
+
+        private void RSVParameters_Load(object sender, EventArgs e)
+        {
+            if (groupBox1 != null)
+            {
+                groupBox1.Enabled = false;
+            }
+
+            cmbProfiles.Enabled = false;
+        }
+    }
+}
+
+
